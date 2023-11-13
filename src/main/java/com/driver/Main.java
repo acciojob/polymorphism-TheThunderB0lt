b314dc953259965b8c77bc8775d7362ea0aedf77
TheThunderB0lt <nikithkumarniki160698@gmail.com>
2023-11-13 21:17:16 +0530

    Code Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,42 +16,47 @@ Observations: This class contains 3 classes with same name, but it complie & run
 package com.driver;
 
 public class Main {
-    // 1. create a class Product inside Main class
-    static class Product {
-        // 2. create a method
+
+    public static void main(String[] args) {
+        // Task 2: Create object of Product in Main function called p
+        Product p = new Product();
+
+        // Task 3: Call method public int product(int x, int y) from Main using Product class object p
+        int result1 = p.product(5, 3);
+        System.out.println("Product of two integers: " + result1);
+
+        // Task 4: Call Overloaded method public int product(int x, int y, int z) from Main using Product class object p
+        int result2 = p.product(2, 3, 4);
+        System.out.println("Product of three integers: " + result2);
+
+        // Task 5: Call Overloaded method public double product(double x, double y) from Main using Product class object p
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Product of two doubles: " + result3);
+    }
+
+    // Task 1: Create a class Product inside Main class
+    public class Product {
+        // Task 3: Create method public int product(int x, int y)
         public int product(int x, int y) {
             return x * y;
         }
 
-        // 4. create a Overloaded method product
+        // Task 4: Create Overloaded method public int product(int x, int y, int z)
         public int product(int x, int y, int z) {
             return x * y * z;
         }
 
-        // 5. create a Overloaded method product
+        // Task 5: Create Overloaded method public double product(double x, double y)
         public double product(double x, double y) {
             return x * y;
         }
     }
-
-    public static void main(String[] args) {
-        // 2. create object of Product in Main function called p
-        Product p = new Product();
-
-        int res1 = p.product(2, 3);
-        System.out.println("Result of method 1 product(x, y): " + res1);
-        
-        int res2 = p.product(2, 3, 4);
-        System.out.println("Result of method 2 product(x, y, z): " + res2);
-        
-        double res3 = p.product(2.3, 3.4);
-        System.out.println("Result of method 3 product(x, y): " + res3);
-
-    }
 }
+
+
 /*
  OUTPUT: 
-Result of method 1 product(x, y): 6
+ Result of method 1 product(x, y): 6
 Result of method 2 product(x, y, z): 24
 Result of method 3 product(x, y): 7.819999999999999
  */
